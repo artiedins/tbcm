87693c3e5a3dc8ca94c25ab6d78003122fdf9eb8
artiedins <107737805+artiedins@users.noreply.github.com>
2022-06-18 09:15:35 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tbcm',
+      version='0.1',
+      description='Ten Bit Color Maps',
+      url='https://github.com/artiedins/tbcm',
+      license='MIT',
+      packages=['tbcm'],
+      install_requires=["matplotlib"],
+     )
