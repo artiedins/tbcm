13468ea283a4505b86f0a486ca04d2ec175876f6
artie <artie@appa.local>
2022-06-19 19:37:35 -0700

    up version 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tbcm",
-    version="0.2",
+    version="0.3",
     description="Ten Bit Color Maps",
     author="Artie Dins",
     url="https://github.com/artiedins/tbcm",
