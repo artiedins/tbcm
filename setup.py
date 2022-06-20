179142fdf7377cd3e1ff8fbfe38430945153cd00
artiedins <107737805+artiedins@users.noreply.github.com>
2022-06-19 17:00:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='tbcm',
       version='0.1',
       description='Ten Bit Color Maps',
+      author='Artie Dins',
       url='https://github.com/artiedins/tbcm',
       license='MIT',
       packages=['tbcm'],
