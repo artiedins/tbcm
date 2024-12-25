d151b99ca30b4dfe68589fbe255b1d00a41cf88d
Artie Dins <artie.dins@github.com>
2024-12-25 16:27:54 -0700

    roll version 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tbcm",
-    version="0.3",
+    version="0.4",
     description="Ten Bit Color Maps",
     author="Artie Dins",
     url="https://github.com/artiedins/tbcm",
